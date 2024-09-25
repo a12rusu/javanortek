dc4efb2ca0886f063013075582c8d36ded0d0d5e
a12rusu <168715489+a12rusu@users.noreply.github.com>
2024-09-25 12:20:23 +0300

    new commit

---
+++ test.java
@@ -0,0 +1,12 @@
+class Test {
+        public static void main(String[] args) {
+            int suma = 0;
+            for (int i = 1; i <= 10; i++) {
+                suma += i;
+            }
+            System.out.println("Suma primelor 10 numere este: " + suma);
+        }
+    }
+    
+
+
